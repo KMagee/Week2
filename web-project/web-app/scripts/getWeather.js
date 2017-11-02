26f95dd5b6e6b8543de80931a9566f10f81b5e29
KMagee <kenny.magee@hotmail.com>
2017-11-02 19:06:26 +0000

    working weather app

--- web-project/web-app/scripts/getWeather.js
+++ web-project/web-app/scripts/getWeather.js
@@ -1,30 +1,38 @@
-// create an instance of an HTTPRequest object
-
-let xhr = new XMLHttpRequest()
-
-xhr.open('get', `http://api.openweathermap.org/data/2.5/weather?q=galway,ie&APPID=48f2d5e18b0d2bc50519b58cce6409f1`)
-xhr.send()
-
 handleForm = ()=>{
-    console.log(frmcity.value)
-    console.log(frmcountry.value)
-
     event.preventDefault()
-    
+    let city=frmcity.value
+    let country=frmcountry.value
 
-    if(xhr.readyState === 4 && xhr.status === 200)
+    let xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function()
     {
-
-        let data = JSON.parse(xhr.responseText)
-        monitor.innerHTML = xhr.responseText
-        
-        let desc=data.weather[0].description
-        let windspeed =  data.wind.speed
-        output.innerHTML =  windspeed + " " + desc 
+        if (this.readyState === 4 && this.status === 200) 
+        {
+            // Action to be performed when the document is read;
+            let data = JSON.parse(xhr.responseText)
+            monitor.innerHTML = xhr.responseText
+            
+            let desc=data.weather[0].description
+            let windspeed =  data.wind.speed
+            let weatherTemp = data.main.temp
+   
+   
+            let report = document.createElement('h4')
+            report.innerHTML =  `Weather Description: ${desc}`
+            weatherOutput.appendChild(report)
+            
+            let temp = document.createElement('h4')
+            temp.innerHTML = `Temperature in ${city} ${country}: ${Math.round(weatherTemp-273)}&deg;`
+            weatherOutput.appendChild(temp)
+   
+            let wind = document.createElement('h4')
+            wind.innerHTML = `Windspeed in ${city} ${country}: ${windspeed}`
+            weatherOutput.appendChild(wind)
+         }  
     }
-    
+     xhr.open('get', `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=48f2d5e18b0d2bc50519b58cce6409f1`)
+     xhr.send()
 }
-
 
 btnGo.addEventListener('click', handleForm)
 
