ba008caddc4d59496e05b2ecf07948261161ceb6
KMagee <magee14@avaya.com>
2017-11-02 16:27:49 +0000

    next2

--- web-project/web-app/scripts/getWeather.js
+++ web-project/web-app/scripts/getWeather.js
@@ -1,24 +1,24 @@
 // create an instance of an HTTPRequest object
 
 let xhr = new XMLHttpRequest()
+
 xhr.open('get', `http://api.openweathermap.org/data/2.5/weather?q=galway,ie&APPID=48f2d5e18b0d2bc50519b58cce6409f1`)
 
 handleForm = ()=>{
     event.preventDefault()
     
 
-    
     if(xhr.readyState === 4 && xhr.status === 200)
     {
 
-        let result = JSON.parse(xhr.responseText)
+        let data = JSON.parse(xhr.responseText)
         monitor.innerHTML = xhr.responseText
         
         let desc=data.weather[0].description
         let windspeed =  data.wind.speed
         output.innerHTML =  windspeed + " " + desc 
-    }
-    xhr.send()
+    }xhr.send()
+    
 }
 
 
