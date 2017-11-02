71d510824a1fb269be5bbed5b34d17db5a01712b
KMagee <magee14@avaya.com>
2017-11-02 16:25:12 +0000

    next

--- web-project/web-app/scripts/getWeather.js
+++ web-project/web-app/scripts/getWeather.js
@@ -1,26 +1,25 @@
 // create an instance of an HTTPRequest object
 
-
+let xhr = new XMLHttpRequest()
+xhr.open('get', `http://api.openweathermap.org/data/2.5/weather?q=galway,ie&APPID=48f2d5e18b0d2bc50519b58cce6409f1`)
 
 handleForm = ()=>{
     event.preventDefault()
-
-    let xhr = new XMLHttpRequest()
-    xhr.open('get', `http://api.openweathermap.org/data/2.5/weather?q=${frmcity.value},${frmcountry.value}&APPID=48f2d5e18b0d2bc50519b58cce6409f1`)
-    let data = JSON.parse(xhr.responseText)
-    monitor.innerHTML = xhr.responseText
     
+
     
-    let userURL = `http://api.openweathermap.org/data/2.5/weather?q=${frmcity.value},${frmcountry.value}&APPID=48f2d5e18b0d2bc50519b58cce6409f1`
-    username.innerHTML = userURL
+    if(xhr.readyState === 4 && xhr.status === 200)
+    {
 
-
-
-   
+        let result = JSON.parse(xhr.responseText)
+        monitor.innerHTML = xhr.responseText
+        
+        let desc=data.weather[0].description
+        let windspeed =  data.wind.speed
+        output.innerHTML =  windspeed + " " + desc 
+    }
+    xhr.send()
 }
-
-
-              
 
 
 btnGo.addEventListener('click', handleForm)
@@ -38,20 +37,6 @@ btnGo.addEventListener('click', handleForm)
 
 // event.preventDefault()
 
-    
-//     if(xhr.readyState === 4 && xhr.status === 200)
-//     {
-
-    xhr.send()
-
-
-//         let desc=data.weather[0].description
-//         let windspeed =  data.wind.speed
-//         output.innerHTML =  windspeed + " " + desc 
-       
-//     }
-
-    
 
 // }
 // btnGo.addEventListener('click', handleData)
